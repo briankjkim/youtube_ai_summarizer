66402855a94d106add029bfe90c617ad4512542e
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-26 22:19:09 -0700

    Modify registerUserAction fn to use jwt and cookies

--- src/data/actions/auth-actions.ts
+++ src/data/actions/auth-actions.ts
@@ -1,5 +1,16 @@
 "use server";
 import { z } from "zod";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { registerUserService } from "../services/auth-service";
+
+const config = {
+  maxAge: 60 * 60 * 24 * 7, // 1 week
+  path: "/",
+  domain: process.env.HOST ?? "localhost",
+  httponly: true,
+  secure: process.env.NODE_ENV === "production",
+};
 
 const schemaRegister = z.object({
   username: z.string().min(3).max(20, {
@@ -14,8 +25,6 @@ const schemaRegister = z.object({
 });
 
 export async function registerUserAction(prevState: any, formData: FormData) {
-  console.log("Hello From Register User Action");
-
   const validatedFields = schemaRegister.safeParse({
     username: formData.get("username"),
     password: formData.get("password"),
@@ -30,8 +39,26 @@ export async function registerUserAction(prevState: any, formData: FormData) {
     };
   }
 
-  return {
-    ...prevState,
-    data: "ok",
-  };
+  const responseData = await registerUserService(validatedFields.data);
+
+  if (!responseData) {
+    return {
+      ...prevState,
+      strapiErrors: null,
+      zodErrors: null,
+      message: "Ops! Something went wrong. Please try again.",
+    };
+  }
+
+  if (responseData.error) {
+    return {
+      ...prevState,
+      strapiErrors: responseData.error,
+      zodErrors: null,
+      message: "Failed to Register.",
+    };
+  }
+
+  cookies().set("jwt", responseData.jwt, config);
+  redirect("/dashboard");
 }
