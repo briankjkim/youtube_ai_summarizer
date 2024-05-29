023b4c8fe1db97351760eab9eb8147f128628360
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-29 12:04:37 -0700

    Change logos and icons for footer items

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -20,8 +20,8 @@ interface FooterProps {
 }
 
 function selectSocialIcon(url: string) {
-  if (url.includes("youtube")) return <YoutubeIcon className="h-6 w-6" />;
-  if (url.includes("twitter")) return <TwitterIcon className="h-6 w-6" />;
+  if (url.includes("gmail")) return <EmailIcon className="h-6 w-6" />;
+  if (url.includes("linkedin")) return <LinkedinIcon className="h-6 w-6" />;
   if (url.includes("github")) return <GithubIcon className="h-6 w-6" />;
   return null;
 }
@@ -72,7 +72,46 @@ function GithubIcon(props: any) {
   );
 }
 
-function TwitterIcon(props: any) {
+// function TwitterIcon(props: any) {
+//   return (
+//     <svg
+//       {...props}
+//       xmlns="http://www.w3.org/2000/svg"
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//     >
+//       <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+//     </svg>
+//   );
+// }
+
+// function YoutubeIcon(props: any) {
+//   return (
+//     <svg
+//       {...props}
+//       xmlns="http://www.w3.org/2000/svg"
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//     >
+//       <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17" />
+//       <path d="m10 15 5-3-5-3z" />
+//     </svg>
+//   );
+// }
+
+function LinkedinIcon(props: any) {
   return (
     <svg
       {...props}
@@ -86,12 +125,14 @@ function TwitterIcon(props: any) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+      <rect width="4" height="12" x="2" y="9" />
+      <circle cx="4" cy="4" r="2" />
     </svg>
   );
 }
 
-function YoutubeIcon(props: any) {
+function EmailIcon(props: any) {
   return (
     <svg
       {...props}
@@ -105,8 +146,9 @@ function YoutubeIcon(props: any) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17" />
-      <path d="m10 15 5-3-5-3z" />
+      <path d="M22 12v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-7" />
+      <path d="M22 6v0a2 2 0 0 0-2-2H4C2.9 4 2 4.9 2 6v0" />
+      <path d="m22 6-10 7L2 6" />
     </svg>
   );
 }
