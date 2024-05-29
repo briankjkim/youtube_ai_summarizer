c3cc5d43a1a1b840975ea66f3df555cf88302b54
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-29 12:04:25 -0700

    Integrate the temp message in Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,16 @@
+import DashboardTempMessage from "@/components/custom/DashboardTempMessage";
 import { LogoutButton } from "@/components/custom/LogoutButton";
+import { getUserMeLoader } from "@/data/services/get-user-me-loader";
 
-export default function DashboardRoute() {
+export default async function DashboardRoute() {
+  const user = await getUserMeLoader();
+  //TODO: Refine the Dashboard in production
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
-      <h1>Dashboard</h1>
-      <LogoutButton />
+      <DashboardTempMessage userName={user.data.username} />
+      <span className=" p-10">
+        <LogoutButton />
+      </span>
     </div>
   );
 }
